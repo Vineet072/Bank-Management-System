03ef74108d6570e75eec606af019933c0e21bbe3
Vineet072 <128561458+Vineet072@users.noreply.github.com>
2024-08-24 18:07:26 +0530

    Add files via upload
    A MERN Stack project , stimulating the bank system with user credentials , loan , bank transfers and feedback email to the developers.
    The use of React , Mongodb and node allow us to make the page interactive.

---
+++ aboutscript.js
@@ -0,0 +1,146 @@
+$(document).ready(function(){
+    $('.p01').click(function(){
+        $(this).css("color","blue").html("I was clicked")
+    })
+})
+
+$(document).ready(function(){
+    $('#b1').hide(0);
+    $('.p01').hide(0);
+    $('#b1').click(function(){
+        $('.p01').hide(500);
+        $('#b1').hide(500);
+        $('#a1').show("slow");
+    });
+    $('#a1').click(function(){
+        $('.p01').show("slow");
+        $('#a1').hide(500);
+        $('#b1').show("slow");
+    });
+})
+
+$(document).ready(function(){
+$('.p02').click(function(){
+$(this).css("color","blue").html("I was clicked")
+})
+})
+
+$(document).ready(function(){
+    $('#b2').hide(0);
+    $('.p02').hide(0);
+$('#b2').click(function(){
+$('.p02').hide(500);
+$('#b2').hide(500);
+$('#a2').show("slow");
+});
+$('#a2').click(function(){
+$('.p02').show("slow");
+$('#a2').hide(500);
+$('#b2').show("slow");
+});
+})
+
+$(document).ready(function(){
+$('.p03').click(function(){
+$(this).css("color","blue").html("I was clicked")
+})
+})
+
+$(document).ready(function(){
+    $('#b3').hide(0);
+    $('.p03').hide(0);
+$('#b3').click(function(){
+$('.p03').hide(500);
+$('#b3').hide(500);
+$('#a3').show("slow");
+});
+$('#a3').click(function(){
+$('.p03').show("slow");
+$('#a3').hide(500);
+$('#b3').show("slow");
+});
+})
+
+$(document).ready(function(){
+$('.p04').click(function(){
+$(this).css("color","blue").html("I was clicked")
+})
+})
+
+$(document).ready(function(){
+    $('#b4').hide(0);
+    $('.p04').hide(0);
+$('#b4').click(function(){
+$('.p04').hide(500);
+$('#b4').hide(500);
+$('#a4').show("slow");
+});
+$('#a4').click(function(){
+$('.p04').show("slow");
+$('#a4').hide(500);
+$('#b4').show("slow");
+});
+})
+
+$(document).ready(function(){
+$('.p05').click(function(){
+$(this).css("color","blue").html("I was clicked")
+})
+})
+
+$(document).ready(function(){
+    $('#b5').hide(0);
+    $('.p05').hide(0);
+$('#b5').click(function(){
+$('.p05').hide(500);
+$('#b5').hide(500);
+$('#a5').show("slow");
+});
+$('#a5').click(function(){
+$('.p05').show("slow");
+$('#a5').hide(500);
+$('#b5').show("slow");
+});
+})
+
+$(document).ready(function(){
+$('.p06').click(function(){
+$(this).css("color","blue").html("I was clicked")
+})
+})
+
+$(document).ready(function(){
+    $('#b6').hide(0);
+    $('.p06').hide(0);
+$('#b6').click(function(){
+$('.p06').hide(500);
+$('#b6').hide(500);
+$('#a6').show("slow");
+});
+$('#a6').click(function(){
+$('.p06').show("slow");
+$('#a6').hide(500);
+$('#b6').show("slow");
+});
+})
+
+$(document).ready(function(){
+$('.p07').click(function(){
+$(this).css("color","blue").html("I was clicked")
+})
+})
+
+$(document).ready(function(){
+    $('#b7').hide(0);
+    $('.p07').hide(0);
+$('#b7').click(function(){
+$('.p07').hide(500);
+$('#b7').hide(500);
+$('#a7').show("slow");
+});
+$('#a7').click(function(){
+$('.p07').show("slow");
+$('#a7').hide(500);
+$('#b7').show("slow");
+});
+})
